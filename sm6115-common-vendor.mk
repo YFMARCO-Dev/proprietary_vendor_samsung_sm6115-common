898a3c362f74ceebd411908f8690697e80524e00
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2026-04-14 19:19:19 +0200

    build(sm6115): cleanup product package assignments
    Fixes syntax errors in the vendor makefile by properly terminating
    package lists and removing a dangling assignment.
    
    - Terminates xtra-daemon entry correctly
    - Moves adreno libraries to a dedicated assignment block
    - Removes erroneous trailing PRODUCT_PACKAGES line

--- sm6115-common-vendor.mk
+++ sm6115-common-vendor.mk
@@ -851,11 +851,12 @@ PRODUCT_PACKAGES += \
     vaultkeeperd \
     vendor.samsung.hardware.security.vaultkeeper@2.0-service \
     wt_rpmb_status \
-    xtra-daemon \
+    xtra-daemon
+
+PRODUCT_PACKAGES += \
     vendor_lib_libEGL_adreno_so \
     vendor_lib_libGLESv2_adreno_so \
     vendor_lib_libq3dtools_adreno_so \
     vendor_lib64_libEGL_adreno_so \
     vendor_lib64_libGLESv2_adreno_so \
-    vendor_lib64_libq3dtools_adreno_so \
-    PRODUCT_PACKAGES +=
+    vendor_lib64_libq3dtools_adreno_so
